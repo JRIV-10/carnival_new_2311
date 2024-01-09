c42cc6ff07bf5ec0d7acfebc4c55ebff27f7d390
Joey Reyes <joeyreyes105@gmail.com>
2024-01-09 08:56:16 -0800

    Can access all info inside the initialize hash

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -7,5 +7,29 @@ RSpec.describe Ride do
 
             expect(ride1).to be_a Ride 
         end
+
+        it 'has a name' do 
+            ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+
+            expect(ride1.name).to eq('Carousel')
+        end
+
+        it 'has a min_height' do 
+            ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+
+            expect(ride1.min_height).to eq(24)
+        end
+
+        it 'has an admission fee' do 
+            ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+
+            expect(ride1.admission_fee).to eq(1)
+        end
+
+        it 'has a exictement level' do 
+            ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+
+            expect(ride1.excitement).to eq(:gentle)
+        end
     end
 end
