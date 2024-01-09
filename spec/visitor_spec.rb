00f5c7b481f81e557aae82adf8af3c29a0c9876e
Joey Reyes <joeyreyes105@gmail.com>
2024-01-09 08:09:39 -0800

    Initial commit, initialize visitor

--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -1,2 +1,11 @@
 require 'spec_helper'
 
+RSpec.describe Visitor do 
+    describe '#initialize' do 
+        it 'exists' do 
+            visitor = Visitor.new('Bruce', 54, '$10')
+
+            expect(visitor).to be_a Visitor
+        end
+    end 
+end 
