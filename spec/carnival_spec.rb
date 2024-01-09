9a7c2b103262945b46cfd78a9dfd009d70046fb2
Joey Reyes <joeyreyes105@gmail.com>
2024-01-09 10:57:00 -0800

    Add most popular ride method

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -30,4 +30,25 @@ RSpec.describe Carnival do
             expect(carnival.rides).to eq([ride3])
         end
     end
+
+    describe '#most_popular_ride' do 
+        it 'returns the most popular ride' do 
+            carnival = Carnival.new(duration: 14)
+            ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+            ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
+            carnival.add_ride(ride3)
+            carnival.add_ride(ride1)
+            visitor1 = Visitor.new('Bruce', 54, '$10')
+            visitor2 = Visitor.new('Tucker', 36, '$5')
+
+            ride1.board_rider(visitor1)
+            ride1.board_rider(visitor2)
+
+            ride3.board_rider(visitor1)
+            ride3.board_rider(visitor2)
+            ride3.board_rider(visitor2)
+
+            expect(carnival.most_popular_ride).to eq(ride3)
+        end
+    end
 end
