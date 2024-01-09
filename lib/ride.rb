5bab54e1ccee1064a3f991c28c3e6a7a01a81af0
Joey Reyes <joeyreyes105@gmail.com>
2024-01-09 08:52:07 -0800

    Create test and initialize for ride

---
+++ lib/ride.rb
@@ -0,0 +1,13 @@
+class Ride 
+    attr_reader :name,
+                :min_height,
+                :admission_fee,
+                :excitement
+                
+    def initialize(ride_data)
+        @name = ride_data[:name]
+        @min_height = ride_data[:min_height]
+        @admission_fee = ride_data[:admission_fee]
+        @excitement = ride_data[:excitement]
+    end
+end
