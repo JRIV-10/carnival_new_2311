6ffe89ce332ef089e12e3e7f099d52f00a8837e5
Joey Reyes <joeyreyes105@gmail.com>
2024-01-09 10:45:06 -0800

    Update carnival tests

--- lib/carnival.rb
+++ lib/carnival.rb
@@ -1,3 +1,9 @@
 class Carnival
+    attr_reader :rides, 
+                :duration
     
+    def initialize(carnival_data)
+        @duration = carnival_data[:duration]
+        @rides = []
+    end
 end
--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -1,9 +1,23 @@
 require 'spec_helper'
 
 RSpec.describe Carnival do 
-    it 'exists' do 
-        carnival = Carnival.new 
+    describe '#initialize' do 
+        it 'exists' do 
+            carnival = Carnival.new(duration: 14) 
 
-        expect(carnival).to be_a Carnival
-    end
+            expect(carnival).to be_a Carnival
+        end
+
+        it 'has rides' do 
+            carnival = Carnival.new(duration: 14)
+
+            expect(carnival.rides).to eq([])
+        end
+
+        it 'has a duration' do 
+            carnival = Carnival.new(duration: 14)
+
+            expect(carnival.duration).to eq(14)
+        end
+    end 
 end
