a1b62f3542d104a4cf84d9487f37921af54795ea
Joey Reyes <joeyreyes105@gmail.com>
2024-01-09 08:13:52 -0800

    Add all test passining for initialize

--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -7,5 +7,29 @@ RSpec.describe Visitor do
 
             expect(visitor).to be_a Visitor
         end
+
+        it 'has a name' do 
+            visitor = Visitor.new('Bruce', 54, '$10')
+
+            expect(visitor.name).to eq('Bruce')
+        end
+
+        it 'has a height' do 
+            visitor = Visitor.new('Bruce', 54, '$10')
+
+            expect(visitor.height).to eq(54)
+        end
+
+        it 'has spending money' do 
+            visitor = Visitor.new('Bruce', 54, '$10')
+
+            expect(visitor.spending_money).to eq('$10')
+        end
+
+        it 'has preferences' do 
+            visitor = Visitor.new('Bruce', 54, '$10')
+
+            expect(visitor.preferences).to eq([])
+        end
     end 
 end 
